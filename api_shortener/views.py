d9cf8b02ce7ee1424d0f54fd5601630afac6a22c
isamarcev <isamarcev1994@gmail.com>
2022-10-29 00:56:23 +0300

    add dev 2

--- api_shortener/views.py
+++ api_shortener/views.py
@@ -1,10 +1,21 @@
+import string
+from random import choice
+
 import pymongo
-from django.shortcuts import render
-from pymongo import MongoClient
 from shortener.settings import env
 # Create your views here.
 
 client = pymongo.MongoClient(env('MONGO_STRING'))
 db = client[env('DB_NAME')]
 collection = db[env('DB_COLLECTION')]
+
+def generate_key():
+    chars = string.digits + string.ascii_letters
+    print(chars)
+    key = ''.join([choice(chars) for _ in range(5)])
+    return key, chars
+
+data_model = { '_id': int, 'url': 'https...', 'key': 'short_url',
+               'expireAt': 'Date', 'IP': 'ip', 'counter': int }
+
 
