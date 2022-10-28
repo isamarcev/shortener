113131138009dde2c85b38fd85d4670bfd59d497
isamarcev <isamarcev1994@gmail.com>
2022-10-28 16:51:20 +0300

    connected to mongoDB

--- api_shortener/views.py
+++ api_shortener/views.py
@@ -1,3 +1,10 @@
+import pymongo
 from django.shortcuts import render
-
+from pymongo import MongoClient
+from shortener.settings import env
 # Create your views here.
+
+client = pymongo.MongoClient(env('MONGO_STRING'))
+db = client[env('DB_NAME')]
+collection = db[env('DB_COLLECTION')]
+
